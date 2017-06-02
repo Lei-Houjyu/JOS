4a55a786092e068a70826fb64a4c581250133dd6
Li Haoyu <Guita_r@sjtu.edu.cn>
2017-06-02 19:36:00 +0800

    pass exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,14 @@
 #include <kern/e1000.h>
+#include <inc/memlayout.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int attach_function(struct pci_func *pcif) {
+    pci_func_enable(pcif);
+    
+    boot_map_region(kern_pgdir, KSTACKTOP, pcif->reg_size[0], pcif->reg_base[0], PTE_PCD | PTE_PWT | PTE_W);
+
+
+
+    return 1;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,10 @@
+#include <kern/pci.h>
+
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+volatile uint32_t *E1000;
+
+int attach_function(struct pci_func *pcif);
 
 #endif	// JOS_KERN_E1000_H
