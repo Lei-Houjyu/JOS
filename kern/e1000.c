359073472a42a0072299934ad54ea92a673aed2e
Li Haoyu <Guita_r@sjtu.edu.cn>
2017-06-05 19:12:09 +0800

    pass exercise 5

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,5 +1,6 @@
 #include <kern/e1000.h>
 #include <inc/memlayout.h>
+#include <inc/string.h>
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
@@ -7,6 +8,33 @@ int attach_function(struct pci_func *pcif) {
     pci_func_enable(pcif);
     
     boot_map_region(kern_pgdir, KSTACKTOP, pcif->reg_size[0], pcif->reg_base[0], PTE_PCD | PTE_PWT | PTE_W);
+    E1000 = (uint32_t *)KSTACKTOP;
+
+    memset(tx_desc_array, 0, sizeof(struct tx_desc) * MAX_TX_DESC_N);
+    memset(tx_pkt_buffer, 0, sizeof(struct tx_pkt)  * MAX_TX_DESC_N);
+    int i;
+    for (i = 0; i < MAX_TX_DESC_N; i++) {
+        tx_desc_array[i].addr = PADDR(tx_pkt_buffer[i].content);
+        tx_desc_array[i].status |= 1;
+    }
+
+    E1000[E1000_TDBAL] = PADDR(tx_desc_array);
+    E1000[E1000_TDBAH] = 0;
+    E1000[E1000_TDLEN] = sizeof(struct tx_desc) * MAX_TX_DESC_N;
+    E1000[E1000_TDH] = 0;
+    E1000[E1000_TDT] = 0;
+    E1000[E1000_TCTL] |= E1000_TCTL_EN;
+    E1000[E1000_TCTL] |= E1000_TCTL_PSP;
+    E1000[E1000_TCTL] |= E1000_TCTL_CT;
+    E1000[E1000_TCTL] |= E1000_TCTL_COLD;
+    E1000[E1000_TIPG]  = 0;
+    E1000[E1000_TIPG] |= E1000_TIPG_IPGT;
+    E1000[E1000_TIPG] |= E1000_TIPG_IPGR1;
+    E1000[E1000_TIPG] |= E1000_TIPG_IPGR2;
+
+
+
+
 
 
 
--- kern/e1000.h
+++ kern/e1000.h
@@ -3,8 +3,54 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+/* Constants */
+#define MAX_PACKET_LEN 1518
+#define MAX_TX_DESC_N  64
+
+/* Registers */
+#define E1000_TDBAL    ( 0x03800 / 4 )  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    ( 0x03804 / 4 )  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    ( 0x03808 / 4 )  /* TX Descriptor Length - RW */
+#define E1000_TDH      ( 0x03810 / 4 )  /* TX Descriptor Head - RW */
+#define E1000_TDT      ( 0x03818 / 4 )  /* TX Descripotr Tail - RW */
+#define E1000_TCTL     ( 0x00400 / 4 )  /* TX Control - RW */
+#define E1000_TCTL_RST    ( 0x00000001 / 4 )    /* software reset */
+#define E1000_TCTL_EN     ( 0x00000002 )    /* enable tx */
+#define E1000_TCTL_BCE    ( 0x00000004 )    /* busy check enable */
+#define E1000_TCTL_PSP    ( 0x00000008 )    /* pad short packets */
+#define E1000_TCTL_CT     ( 0x00000ff0 )    /* collision threshold */
+#define E1000_TCTL_COLD   ( 0x003ff000 )    /* collision distance */
+#define E1000_TCTL_SWXOFF ( 0x00400000 )    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    ( 0x00800000 )    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC   ( 0x01000000 )    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   ( 0x02000000 )    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   ( 0x10000000 )    /* Multiple request support */
+#define E1000_TIPG       ( 0x00410 / 4 )  /* TX Inter-packet gap -RW */
+#define E1000_TIPG_IPGT  ( 0xA )          // Transmit Time
+#define E1000_TIPG_IPGR1 ( 0x4 << 10 ) // IPG Receive Time 1
+#define E1000_TIPG_IPGR2 ( 0x6 << 20 ) // IPG Receive Time 2
+
 volatile uint32_t *E1000;
 
+/* Data Structures*/
+struct tx_desc {
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t status;
+    uint8_t css;
+    uint16_t special;
+} __attribute__((packed));
+
+struct tx_pkt {
+    uint8_t content[MAX_PACKET_LEN];
+};
+
+struct tx_desc tx_desc_array[MAX_TX_DESC_N] __attribute__((aligned(16)));
+struct tx_pkt tx_pkt_buffer[MAX_TX_DESC_N] __attribute__((aligned(16)));
+
+/* Functions */
 int attach_function(struct pci_func *pcif);
 
 #endif	// JOS_KERN_E1000_H
